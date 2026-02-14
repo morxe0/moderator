bc629ded75a8b21af87733446545f30f3a21bc6b
max <mlhader@outlook.com>
2026-02-14 13:54:35 -0500

    Implement message normalization and trust score updates
    Added a normalizer class to replace specific characters in user messages for moderation purposes. Updated trust score calculation and added checks for user offenses.

--- moderator.py
+++ moderator.py
@@ -5,6 +5,8 @@ import time
 from discord.ext import commands
 from datetime import datetime, timedelta
 
+
+WEEK = 604800
 UPDATE_LOG = "Moderator v1.0.0 beta by Morxe0 \nAdded permission checking on '/kill', '/revive', and '/put_to_sleep'."
 
 # CONTENT WARNING: the following moderation script may reference words that are extremely harmful in social contexts for the pure purpose of moderation. if you feel triggered by any of the content, speak to someone about it.
@@ -47,12 +49,75 @@ def create_mem_(user: discord.Member):
     Memory[str(user.id)]["trust_score"] = 1.0
     _save()
 
+class normalizer:
+    replacement_table = [
+        {
+            "r": "1",
+            "v": "i"
+        },
+        {
+            "r": "¡",
+            "v": "i"
+        },
+        {
+            "r": "@",
+            "v": "a"
+        },
+        {
+            "r": "|(",
+            "v": "k"
+        },
+        {
+            "r": "!",
+            "v": "i"
+        },
+        {
+            "r": "|",
+            "v": "i"
+        },
+        {
+            "r": "&",
+            "v": "8"
+        },
+        {
+            "r": "0",
+            "v": "o"
+        },
+        {
+            "r": "9",
+            "v": "g"
+        },
+        {
+            "r": "3",
+            "v": "e"
+        },
+        {
+            "r": "$",
+            "v": "s"
+        },
+        {
+            "r": "+",
+            "v": "t"
+        },
+        {
+            "r": "\\/",
+            "v": "v"
+        }
+    ] # WILL be expanded later after testing
+    @classmethod
+    def normalized(self, txt: str):
+        t = txt.strip()
+        for r in self.replacement_table:
+            t = t.replace(r["r"], r["v"])
+        return t
+
+
 def calc_trust_score(offenses: list):
     trust = 1.0
 
     for offense in offenses:
         age = time.time() - offense["ts"]
-        weeks = age // 604800
+        weeks = age // WEEK
         decay = weeks * 0.1
 
         impact = max(0, offense["trust_score_impact"] - decay)
@@ -75,19 +140,75 @@ async def tell_off_offending_user(user: discord.Member, word: str, category: str
         o.append({
             "trust_score_impact": 0.5,
             "ts": time.time(),
-            "reason": "Said extremely bad word(s).",
-            "issuer": bot.user.display_name
+            "reason": "Encouraged another user to kill/harm themselves.",
+            "issuer": bot.user.id
+        })
+        Memory[str(user.id)]["offenses"] = o
+        Memory[str(user.id)]["trust_score"] = calc_trust_score(o)
+        _save()
+    if category == "very_very_bad":
+        await user.dm_channel.send(f"# Your message could not be sent.\nYour message that read as follows:\n >{msg}\nCould not be sent as it contains potentially offensive/sexual content. example: {word}. Words like that are extremely offensive.\n-# if you belive this is a mistake, please contact the owner of the server. Thank you.")
+        o: list = Memory[str(user.id)]["offenses"]
+        o.append({
+            "trust_score_impact": 0.35,
+            "ts": time.time(),
+            "reason": "Said extremely offensive/sexual word(s).",
+            "issuer": bot.user.id
+        })
+        Memory[str(user.id)]["offenses"] = o
+        Memory[str(user.id)]["trust_score"] = calc_trust_score(o)
+        _save()
+    if category == "bad":
+        await user.dm_channel.send(f"# Your message could not be sent.\nYour message that read as follows:\n >{msg}\nCould not be sent as it contains potentially offensive/sexual content. example: {word}. Words like that may be offensive to some groups.\n-# if you belive this is a mistake, please contact the owner of the server. Thank you.")
+        o: list = Memory[str(user.id)]["offenses"]
+        o.append({
+            "trust_score_impact": 0.1,
+            "ts": time.time(),
+            "reason": "Said mildly offensive/sexual word(s).",
+            "issuer": bot.user.id
         })
         Memory[str(user.id)]["offenses"] = o
         Memory[str(user.id)]["trust_score"] = calc_trust_score(o)
         _save()
 
+async def _checks(user: discord.Member, authorizer: discord.Member, guild: discord.Guild):
+    if not authorizer.guild_permissions.administrator:
+        return 1
+    ts = calc_trust_score(Memory[str(user.id)]["offenses"])
+    Memory[str(user.id)]["trust_score"] = ts
+    if ts <= 0.01:
+        await user.ban()
+        if not user.dm_channel:
+            await user.create_dm()
+        await user.dm_channel.send(f"Due to your actions, the moderators of {guild.name} chose to ban you. If you feel this was a mistake, feel free to contact the owners of the server... Please note that in the event that the moderators of this server unban you, you will be notified.")
+    elif ts <= 0.5:
+        Memory[str(user.id)]["trusted"] = True
+    return 0
+
 @bot.event
 async def on_ready():
     # Sync the slash commands to your guild
-    guild = discord.Object(id=GUILD_ID)
+    guild: discord.Guild = discord.Object(id=GUILD_ID)
     await bot.tree.sync(guild=guild)
     print(f"Logged in as {bot.user} and commands synced!")
+    await bot.change_presence(
+        status=discord.Status.idle,
+        activity=discord.CustomActivity(name="moderating")
+    )
+
+@bot.tree.command(
+    name="runchecksonall",
+    description="runs the checks on all users.",
+    guild=discord.Object(id=GUILD_ID)
+)
+async def runchecksonall(
+    interaction: discord.Interaction
+):
+    for user in interaction.guild.members:
+        resp = await _checks(user, interaction.user, interaction.guild)
+        if resp == 1:
+            await interaction.response.send_message("You do not have proper permissions.")
+            return
 
 @bot.event
 async def on_member_join(member):
@@ -98,7 +219,7 @@ async def on_member_join(member):
 async def on_message(msg: discord.Message):
     if str(msg.author.id) not in Memory:
         create_mem_(msg.author)
-    txt = msg.content
+    txt = normalizer.normalized(msg.content)
     channel = msg.channel
     if not msg.author.bot:
         for word in Triggerwords["absolute_worst"]:
@@ -107,6 +228,21 @@ async def on_message(msg: discord.Message):
                 await msg.delete()
                 await tell_off_offending_user(author, word, "absolute_worst", txt)
                 await channel.send(f"a message from {author.mention} was deleted by {bot.user.mention}.")
+                break
+        for word in Triggerwords["very_very_bad"]:
+            if word.lower() in txt.lower():
+                author = msg.author
+                await msg.delete()
+                await tell_off_offending_user(author, word, "very_very_bad", txt)
+                await channel.send(f"a message from {author.mention} was deleted by {bot.user.mention}.")
+                break
+        for word in Triggerwords["bad"]:
+            if word.lower() in txt.lower():
+                author = msg.author
+                await msg.delete()
+                await tell_off_offending_user(author, word, "bad", txt)
+                await channel.send(f"a message from {author.mention} was deleted by {bot.user.mention}.")
+                break
 
 
 @bot.tree.command(
@@ -180,7 +316,7 @@ async def revive(
                 await user.send(f"You are unbanned from {interaction.guild.name}.")
                 if send_invite:
                     invite = await interaction.channel.create_invite(
-                        max_age=604800,
+                        max_age=WEEK,
                         max_uses=1
                     )
                     await user.send(
@@ -208,8 +344,10 @@ async def revive(
 async def put_to_sleep(
     interaction: discord.Interaction,
     user: discord.Member, # who to timeout
-    time: float # how long
+    time: discord.Optional[float] # how long
 ):
+    if not time:
+        time = 60
     if not interaction.user.guild_permissions.moderate_members:
         await interaction.response.send_message(
             "You do not have permission to timeout other users.",
